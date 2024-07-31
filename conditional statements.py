93841ce6637de4ed55b086a214966b28bb78eeee
Navya Sri Polavarapu <navyasripolavarapu29@gmail.com>
2024-07-31 22:12:43 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,29 @@
+##a = int(input("Enter the number:"))
+##
+##if a%2==0:
+##     print('Even')
+##
+##a = int(input("Enter a:"))
+##b = int(input("Enter b:"))
+##c = int(input("Enter c:"))
+##if a>b and a<c:
+##    print('a is largest')
+##if b<a and b<c:
+##    print('b is largest')
+##if c>a and c>b:
+##    print('c is largest')
+
+##a = int(input("Enter the number:"))
+##
+##if a%2==0:
+##     print('Even')
+##else:
+##    print('odd')
+
+    
+age = int(input("Enter the age :"))
+if age >=18:
+    print('you are eligible for vote')
+else:
+    print(' you are not eligible for vote')
+    
