4da4912c1a907430e9c655ecb0d82f2b9a7ad12d
Navya Sri Polavarapu <navyasripolavarapu29@gmail.com>
2024-07-31 22:31:10 +0530

    Add files via upload

---
+++ if else condition.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+if a>5:
+    print('a is greater than 5')
+    if b>2:
+        print('b is not greater than 2')
+    else:print('b is not greater than 2')
+else:
+    print('a is not greater than 5')
+         
